f8292ccefe4c568f62dc4d244462cf15771df778
Stephen Arsenault <stephen.arsenault17@gmail.com>
2016-05-26 15:52:37 -0400

    [PD1 & pd1_tb] created folder and tb update

--- PD1/pd1_tb.sv
+++ PD1/pd1_tb.sv
@@ -64,6 +64,43 @@ module tb_BubbleSort;
 	
 endmodule
 
+/* tb_SumArray
+* Loads SumArray.x and tests reads of multiple words.
+*/
+module tb_SumArray;
+	logic clk, read, busy, en;
+	logic [31:0] addr, din, dout;
+	logic [1:0] size;
+
+    memory #(.benchmark("SumArray.x"), .depth(2**20)) mem(.clk(clk),
+		.addr(addr), .data_in(din), .data_out(dout),
+		.access_size(size), .rd_wr(read), .busy(busy), .enable(en));
+
+    initial $monitor("time %3d, addr %8h, data %8h, busy %1b", $time, addr,
+		dout, busy);
+ 
+    initial begin
+        clk = 1; forever #5 clk = ~clk;
+    end
+
+    initial begin
+        addr <= 32'h80020000; size <= 0; read <= 1; en <= 0;
+		#10 en <= 1;
+		#10 size <= 1;
+		#40 size <= 2;
+		#80 size <= 3;
+		#160 read <= 0; size <= 1; din <= 32'haaaaeeee;
+		#10 din <= 32'haaaa0000;
+		#10 din <= 32'h0000eeee;
+		#10 din <= 32'ha0a0e0e0;
+		#10 read <= 1; size <= 0;
+		#20 $stop; //$finish if using iverilog
+    end
+
+    initial $dumpvars(0, tb_BubbleSort); // for iverilog+gtkwave
+	
+endmodule
+
 /* tb_SimpleAdd
 * Loads SimpleAdd.x and tests reads and writes of single words.
 */
